927342d141a07d5e9d96597c81908bc99264f407
chkue2 <chkue0924@gmail.com>
2022-01-09 14:17:09 +0900

    mobile update

--- resource/js/index.js
+++ resource/js/index.js
@@ -1,3 +1,13 @@
 $(document).on('click', '#footerApp', function(){
   $('.footer__app').addClass('on')
 })
+
+$(document).on('click', '.header__menu', function(){
+  $('.side').addClass('on')
+  $('.side__container').addClass('on')
+})
+
+$(document).on('click', '.side__bg, .side__close', function(){
+  $('.side').removeClass('on')
+  $('.side__container').removeClass('on')
+})
